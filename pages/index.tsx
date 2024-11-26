1cf9ec38dd2dc4410212ff4cec1310555f99b6ee
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-26 22:12:19 +0800

    rename import file and tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import Hero from "../components/home-page/hero";
+import Hero from "../components/home-page/Hero";
 import FeaturedPosts from "../components/home-page/FeaturedPosts";
 
 function HomePage() {

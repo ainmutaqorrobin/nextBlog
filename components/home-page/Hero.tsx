0fa90b347f8df78b72a80c268b92a09d3254034f
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-27 07:37:55 +0800

    edit Hero component and add profile image

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import styles from "./Hero.module.css";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/images/site/robin.png"
+          alt="Robin profile image"
+          height={300}
+          width={300}
+        />
+      </div>
+      <h1>Hi, I'm Robin</h1>
+      <p>
+        This is my blog about web development, my current paces is focusing on
+        any JavaScript web framework such as React, Angular, Express etc.
+      </p>
+    </section>
+  );
 }
 
 export default Hero;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,7 +2,7 @@ interface PostPageProps {
   slug: string;
 }
 function PostPage({ slug }: PostPageProps) {
-  return <div>PostPage</div>;
+  return <div>{slug} page.</div>;
 }
 
 export default PostPage;

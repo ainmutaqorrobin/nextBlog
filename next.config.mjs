16dfa678092236e1fb289394467638defe7bcf0a
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-12-08 17:13:49 +0800

    added different env based on phase

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
-const nextConfig = {
-  env: {
-    mongodb_username: "robin",
-    mongodb_password: "robin",
-    mongodb_cluster: "cluster0",
-    mongodb_database: "nextBlog",
-  },
+import { PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants.js";
+
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "robin",
+        mongodb_password: "robin",
+        mongodb_cluster: "cluster0",
+        mongodb_database: "nextBlog-development",
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: "robin",
+      mongodb_password: "robin",
+      mongodb_cluster: "cluster0",
+      mongodb_database: "nextBlog",
+    },
+  };
 };
 
 export default nextConfig;
